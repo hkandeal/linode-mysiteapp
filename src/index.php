9f5b12fb9501342c3315eb92ffa966090b4122d2
Hossam Kandel <hossam20052@hotmail.com>
2024-04-17 17:42:13 +0400

    commit source code

---
+++ src/index.php
@@ -0,0 +1,133 @@
+<?php
+
+//echo "Hello, World from Docker! <br>";
+//echo "Hello ECS! <br>";
+//echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My CV</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+</head>
+
+<body>
+    <header>
+        <img src="res/info.png" alt="" class="profile-photo" width="150" height="150">
+        <h1>Hossam Kandel Mohamed</h1>
+        <p>Solutions Architect</p>
+        <div class="social-links">
+            <a href="https://www.linkedin.com/in/hossamkandel/" target="_blank"><i class="fab fa-linkedin"></i>
+                LinkedIn</a>
+            <a href="https://github.com/hkandeal" target="_blank"><i class="fab fa-github"></i> GitHub</a>
+            <!-- Add more social links as needed -->
+        </div>
+    </header>
+
+    <section id="about">
+        <h2>About Me</h2>
+        <div class="about-item">
+            <div class="icon-placeholder"><i class="fa fa-address-book"></i></div>
+            <p>A creative and versatile Information Technology professional with over 15 years of experience in digital
+                solutions, enterprise integration, business automation and content management.
+                Architecting and developing applications and managing teams in the web and mobile arena. With hands-on
+                experience in multiple technology stacks, agile transformations and an in-depth understanding of the web
+                and
+                mobile industries and architectures across all major platforms
+
+                <br><br> Digital Transformation, Solution Architecture, Project Management, Agile Methodologies,
+                Leadership,
+                Web
+                and
+                Mobile Technologies, Java Spring Boot, Java Spring Cloud , Continuous Integration and ,Continuous
+                Delivery
+                (CI/CD), DevOps ,OAuth 2, Kong API Gateway Solace, Couchbase ,AWS
+                K8s/EKS, OpenShift, Systems Integration, Enterprise Architecture, API Design/Development, E-services
+                Businesses, E-commerce Businesses, Business Workflow Automation, Enterprise Content Management
+                Microservices
+                and SOA Architecture.
+            </p>
+    </section>
+
+    <section id="experience">
+        <h2>Experience</h2>
+        <div class="experience-item">
+            <div class="company-icon"><i class="fas fa-building"></i></div>
+        </div>
+
+        <div class="experience-item">
+            <h3>Senior Solutions Architect</h3>
+            <p><u>Backbase, 2023 - Present</u></p>
+            <ul>
+
+            </ul>
+        </div>
+        <div class="experience-item">
+            <h3>Solutions Architect</h3>
+            <p><u>Emirates Group, 2021 - 2023</u></p>
+            <ul>
+                <li>• Working with the enterprise, lead architects and business on defining architectural enablers,
+                    transitions phases and solutions.</li>
+                <li>• Introducing large set of architectural solutions and ADRs to address Mobile App and booking engine
+                    technical vision of containerization and modernization roadmap.</li>
+                <li>• Working with features teams daily activities, solution refinement, technical design, sprints and
+                    PI
+                    planning.</li>
+                <li>• Participating into enterprise APIs reviews and guidelines and leveraging omni-channel enterprise
+                    services.</li>
+                <li>• Conducting architecture review on existing systems and frameworks.</li>
+                <li>• Participating into migrating legacy systems from on-prem data center into AWS cloud
+                    infrastructure.
+                </li>
+            </ul>
+        </div>
+        <div class="experience-item">
+            <h3>Digital Business Solutions Technical Architect</h3>
+            <p>
+                <u>GBM, 2015 - 2021</u>
+            </p>
+            <ul>
+                <li>• Creating systems high/low level design, building right delivery team structure
+                <li> • Working closely on the ground with the implementation teams on the technical daily activities,
+                    make sure the
+                    evolving solutions match the design, required quality attributes and best practices. These including
+                    code review,
+                    frequent system demos and design sessions.
+                <li> • Evaluating project platforms/technology stack and best methodology for implementation delivery.
+                <li> • Assisting project managers in capturing a detailed project plan, providing work breakdown and
+                    tasks dependencies.
+                <li> • Conduct and actively participate in stakeholder discussions to identify business and technical
+                    requirements, seek
+                    consensus, and evaluate alternative business and technical solutions.
+                <li> • Participating in pre-sales activities such as preparing client demos, POCs and reviewing
+                    technical
+                    RFPs.
+                <li> • Actively managing customer relationships
+            </ul>
+        </div>
+        <!-- Additional experience items can be added here -->
+    </section>
+
+    <section id="education">
+        <h2>Education</h2>
+        <div class="about-item">
+            <div class="icon-placeholder"><i class="fa fa-university"></i></div>
+            <div class="education-item">
+                <h3>Bachelor of Science in Computer Science</h3>
+                <p>Cairo university, Egypt 2002/2006</p>
+            </div>
+    </section>
+
+
+    <footer>
+        <p>&copy; 2024 Hossam Kandel Mohamed</p>
+    </footer>
+</body>
+
+</html>
