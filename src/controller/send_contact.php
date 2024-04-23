84fc8f871235a2066166e3ce61330f94378d50cc
Hossam Kandel <hossam20052@hotmail.com>
2024-04-24 00:51:17 +0400

    db fix

--- src/controller/send_contact.php
+++ src/controller/send_contact.php
@@ -88,6 +88,15 @@
         $password = "rootpassword";
         $dbname = "mysite_db";
 
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=myDB", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+
         // Attempt to connect to the database
         // Create connection
         $conn = new mysqli($host, $username, $password, $dbname);
