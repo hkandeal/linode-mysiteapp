873f92d13b0c78cd03cefedd7c577040fa94a8b3
Hossam Kandel <hossam20052@hotmail.com>
2024-04-29 20:31:11 +0400

    update images

--- public/blog.php
+++ public/blog.php
@@ -5,22 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Under Construction</title>
-    <link rel="apple-touch-icon" sizes="57x57" href="public/res/icons/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="public/res/icons/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="public/res/icons/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="public/res/icons/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="public/res/icons/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="public/res/icons/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="public/res/icons/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="public/res/icons/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="public/res/icons/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="public/res/icons/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="public/res/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="public/res/icons/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="public/res/icons/favicon-16x16.png">
-    <link rel="manifest" href="public/res/icons/manifest.json">
+    <link rel="stylesheet" href="/public/style/home-style.css">
+    <link rel="apple-touch-icon" sizes="57x57" href="/public/res/icons/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/public/res/icons/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/public/res/icons/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/public/res/icons/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/public/res/icons/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/public/res/icons/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/public/res/icons/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/public/res/icons/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/res/icons/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/public/res/icons/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/public/res/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/public/res/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/public/res/icons/favicon-16x16.png">
+    <link rel="manifest" href="/public/res/icons/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="public/res/icons/ms-icon-144x144.png">
+    <meta name="msapplication-TileImage" content="/public/res/icons/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <style>
         body {
@@ -56,7 +57,7 @@
 
 <body>
     <div class="container">
-        <img src="public/res/under-construcation.jpg" alt="Under Construction" class="construction-image">
+        <img src="/public/res/under-con.png" alt="Under Construction" class="construction-image">
         <h1>Under Construction</h1>
         <p>We're sorry, but this page is currently under construction.</p>
         <p>Please check back later.</p>
